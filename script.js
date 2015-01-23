c347543035fbc6834913cb671c176468f8ed3488
LilRead <er368@cam.ac.uk>
2015-01-23 14:27:40 +0000

    added images

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //Flirty greeting
-var greeting="Hello ";
-var name = prompt("Oh hey there, what's your name? ;) xoxo")
-var winky=" ;) xx";
-var greeting_message = greeting + name + winky;
-alert (greeting_message);
 
+bonjour();
+
+function bonjour(greeting) {
+    var greeting = "Hello ";
+    var name = prompt("Oh hey there, what's your name? ;) xoxo")
+    var winky = " ;) xx";
+    var greeting_message = greeting + name + winky;
+    alert(greeting_message);
+}
