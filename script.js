7aeaafb4919036c33947708c2d127d32d625b82b
LilRead <er368@cam.ac.uk>
2015-01-23 14:22:40 +0000

    2pm Fri 23rd

---
+++ script.js
@@ -0,0 +1,7 @@
+//Flirty greeting
+var greeting="Hello ";
+var name = prompt("Oh hey there, what's your name? ;) xoxo")
+var winky=" ;) xx";
+var greeting_message = greeting + name + winky;
+alert (greeting_message);
+
